f30d77a52dd671600228325c303d8bf7bc3b2d41
Lola Marrero <110120745+lola831@users.noreply.github.com>
2024-03-07 13:07:44 -0800

    Solved 'tree min value' (.py)

---
+++ tree_min_value/tree_min_value.py
@@ -0,0 +1,21 @@
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+​
+def tree_min_value(root):
+  min = float('inf')
+  stack = [root]
+  while stack:
+    curr = stack.pop()
+    if curr.val < min:
+      min = curr.val
+    if curr.left:
+      stack.append(curr.left)
+    if curr.right:
+      stack.append(curr.right)
+  return min
+  
+    if target == curr.val:
