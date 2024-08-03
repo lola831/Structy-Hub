88f8dfcd29932dc440cc8a0f52d92e4195631574
Lola Marrero <110120745+lola831@users.noreply.github.com>
2024-08-03 08:35:16 -0700

    Solved 'sum numbers recursive' (.py)

--- sum_numbers_recursive/sum_numbers_recursive.py
+++ sum_numbers_recursive/sum_numbers_recursive.py
@@ -10,7 +10,7 @@ def sum_numbers_recursive(numbers):
 
   if len(numbers) == 0:
     return 0
-  print(numbers[1:])
+  print(numbers[2:])
   return numbers[0] + sum_numbers_recursive(numbers[1:])
   
   
